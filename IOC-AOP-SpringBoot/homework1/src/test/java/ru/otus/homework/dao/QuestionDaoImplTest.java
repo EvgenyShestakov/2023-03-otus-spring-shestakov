d28b8d37c16359fb68344fb540500ba210b8ef6f
evgeny.shestakov <evgeniy.shestakov@gost-group.com>
2023-04-05 20:08:52 +0300

    Homework1 Исправил тест.

--- IOC-AOP-SpringBoot/homework1/src/test/java/ru/otus/homework/dao/QuestionDaoImplTest.java
+++ IOC-AOP-SpringBoot/homework1/src/test/java/ru/otus/homework/dao/QuestionDaoImplTest.java
@@ -1,28 +1,27 @@
 package ru.otus.homework.dao;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.homework.domain.Answer;
+import ru.otus.homework.domain.Question;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
-public class QuestionDaoImplTest {
+class QuestionDaoImplTest {
     @Mock
     private QuestionDaoImpl questionDao;
 
-//    private QuestionServiceImpl questionService;
-//
-//    @BeforeEach
-//    void setUp() {
-//        questionService = new QuestionServiceImpl(questionDao);
-//    }
-//
-//    @Test
-//    void getAllMustHaveSizeOne() {
-//        Map<String, Option> optionMap = Map.of("1", new Option("Answer1", true));
-//        given(questionDao.findAll())
-//                .willReturn(List.of(new Question("Question", optionMap)));
-//        assertThat(questionService.findAll()).hasSize(1);
-//    }
+    @Test
+    void getAllMustHaveSizeOne() {
+        List<Answer> answers = List.of(new Answer(1, "Answer1", true));
+        given(questionDao.findAll())
+                .willReturn(List.of(new Question("Question", answers)));
+        assertThat(questionDao.findAll()).hasSize(1);
+    }
 }
